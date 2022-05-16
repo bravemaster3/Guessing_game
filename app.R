b08ad236e41b835a4ae40c29befd9b46a32af345
Caren Koli <carenkoli@gmail.com>
2022-05-16 17:27:58 +0300

    Update app.R
    #title and background changes

--- app.R
+++ app.R
@@ -1,13 +1,18 @@
 library(shiny)
+library(bslib)
 
 ui <- fluidPage(
-  numericInput("guess_number", 
-               label=h3("Guess a number between 1 and 10"),
-               value=0),
-  actionButton("BtnPlay","PLAY"),
-  uiOutput("result"),
-  uiOutput("generated_num")
-)
+  theme =  bslib::bs_theme(bootswatch = "solar", bg = "#e6e6e6",fg = "#800000"),
+  titlePanel("Guessing number game"),
+  sidebarPanel(
+    numericInput("guess_number", 
+                 label=h5("Guess a number between 1 and 10"),
+                 value=0),
+    actionButton("BtnPlay","PLAY"),
+    uiOutput("result"),
+    uiOutput("generated_num")
+    
+  ))
 
 server <- function(input, output, session) {
   observeEvent(input$BtnPlay,{
@@ -22,9 +27,11 @@ server <- function(input, output, session) {
       )
     }
     
+    
     output$generated_num <- renderUI(paste("The correct number is: ",gen_number))
   })
 }
 
 shinyApp(ui, server)
+
 
